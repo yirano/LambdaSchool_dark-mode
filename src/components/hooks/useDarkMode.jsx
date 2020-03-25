da56ab0a7f9ac2d98a8126bfbfac69e7c0f0f480
yirano <yirano@protonmail.ch>
2020-03-24 23:44:21 -0400

    Delete redundant code

--- src/components/hooks/useDarkMode.jsx
+++ src/components/hooks/useDarkMode.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
 export const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage("dark-mode")
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', '')
 
   useEffect(() => {
     darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
