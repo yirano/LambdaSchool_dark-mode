f9036e996e3c7c78a5840e9241f6048b2c810b0a
yirano <yirano@protonmail.ch>
2020-05-12 18:29:41 -0400

    Refactor code - remove useEffect

--- src/components/hooks/useDarkMode.jsx
+++ src/components/hooks/useDarkMode.jsx
@@ -1,12 +1,9 @@
-import { useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
 export const useDarkMode = (key, initialValue) => {
   const [darkMode, setDarkMode] = useLocalStorage(key, initialValue)
 
-  useEffect(() => {
-    darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
-  }, [darkMode])
+  darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
 
   return [darkMode, setDarkMode]
 }
